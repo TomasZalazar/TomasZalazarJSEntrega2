ca338a4968c42d5b297e75dfe3856a5c36c55058
TomasZalazar <locoarts123@gmail.com>
2023-10-09 23:50:22 -0300

    ultimos arreglos y cambios en las funciones, listo para entregar

--- entrega.js
+++ entrega.js
@@ -1,80 +1,75 @@
 
-
-const blocDeNotas = [];
+const blocDeTareas = [];
 let opcion = "";
+
 // Función para obtener la fecha y hora actual en la zona horaria local del usuario
 function obtenerFechaHoraLocal() {
     const fechaActual = new Date();
     const formatoFechaHoraLocal = fechaActual.toLocaleString();
     return formatoFechaHoraLocal;
 }
-// Función para agregar una nueva idea al array de blocDeNotas con fecha y hora local
-function agregarNotaNueva(nuevaNota) {
+
+// Función para agregar una nueva tarea al bloc de tareas con fecha y hora local
+function agregarTareaNueva(nuevaTarea) {
     const fechaHoraLocal = obtenerFechaHoraLocal();
-    const notaConFecha = `${fechaHoraLocal}: ${nuevaNota}`;
-    blocDeNotas.push(notaConFecha);
-    console.log("La NOTA NUEVA agregada es: " + notaConFecha);
+    const tareaConFecha = `Nueva tarea agregada el ${fechaHoraLocal}:\n\n${nuevaTarea}`;
+    blocDeTareas.push(tareaConFecha);
+    alert("¡Nueva tarea agregada!");
 }
-//funcion para editar una idea de nuestro arrray de blocDeNotas
-function editarIdea(i, editarNota) {
-    const indiceArreglo = i - 1;
-    if (indiceArreglo >= 0 && indiceArreglo < blocDeNotas.length) {
+
+// Función para editar una tarea en el bloc de tareas
+function editarTarea(indice, nuevaTarea) {
+    const indiceArreglo = indice - 1;
+    if (indiceArreglo >= 0 && indiceArreglo < blocDeTareas.length) {
         const fechaHoraLocal = obtenerFechaHoraLocal();
-        blocDeNotas[indiceArreglo] = `${fechaHoraLocal}: ${editarNota}`;
-        console.log(`La nueva nota EDITADA en la posición ${i} es: ${editarNota}`);
+        blocDeTareas[indiceArreglo] = `${fechaHoraLocal}: ${nuevaTarea}`;
+        alert(`Tarea en la posición ${indice} editada.`);
     } else {
-        console.log("El índice ingresado es incorrecto");
+        alert("El número de tarea ingresado es incorrecto.");
     }
 }
-//funcion para eliminar una idea agregada a nuestro array de blocDeNotas
-function eliminarIdea(i) {
-    const indiceArreglo = i - 1;
-    if (indiceArreglo >= 0 && indiceArreglo < blocDeNotas.length) {
-        const notaEliminada = blocDeNotas.splice(indiceArreglo, 1)[0];
-        console.log("La idea fue eliminada: " + notaEliminada);
+
+// Función para eliminar una tarea del bloc de tareas
+function eliminarTarea(indice) {
+    const indiceArreglo = indice - 1;
+    if (indiceArreglo >= 0 && indiceArreglo < blocDeTareas.length) {
+        const tareaEliminada = blocDeTareas.splice(indiceArreglo, 1)[0];
+        alert(`¡Tarea eliminada: ${tareaEliminada}!`);
     } else {
-        console.log("El índice ingresado es incorrecto");
+        alert("El número de tarea ingresado es incorrecto.");
     }
 }
-// Función para mostrar todas las ideas en el bloc de notas
-function mostrarTodasLasIdeas() {
-    console.log("Todas las ideas en el bloc de notas:");
-    blocDeNotas.forEach((idea, index) => {
-        console.table(`${index + 1}. ${idea}`);
+
+// Función para mostrar todas las tareas en el bloc de tareas
+function mostrarTodasLasTareas() {
+    let mensaje = "Todas las tareas en el bloc de tareas:\n\n";
+    blocDeTareas.forEach((tarea, index) => {
+        mensaje += `${index + 1}. ${tarea}\n`;
     });
+    alert(mensaje);
 }
-// Bucle para ofrecer opciones al usuario
-while (opcion !== "5") {
-    alert(`ELEGIR UNA OPCIÓN:
-    1. Agregar tarea
+
+do {
+    opcion = prompt(`ELEGIR UNA OPCIÓN:
+    1. Agregar Tarea
     2. Editar Tarea
     3. Eliminar Tarea
-    4. Mostrar todas las tareas
-    5. Salir del programa`);
+    4. Mostrar Todas las Tareas
+    5. Salir del Programa`);
     
-    opcion = prompt("Ingrese la opción a realizar");
-
-    switch (opcion) {
-        case "1":
-            const nuevaTarea = prompt("Ingrese una nueva tarea:");
-            agregarNotaNueva(nuevaTarea);
-            break;
-        case "2":
-            const indiceEditar = parseInt(prompt("Ingrese el índice de la tarea a editar:"));
-            const nuevaNotaEditar = prompt("Ingrese la nueva tarea:");
-            editarIdea(indiceEditar, nuevaNotaEditar);
-            break;
-        case "3":
-            const indiceEliminar = parseInt(prompt("Ingrese el índice de la tarea a eliminar:"));
-            eliminarIdea(indiceEliminar);
-            break;
-        case "4":
-            console.table(mostrarTodasLasIdeas());
-            break;
-        case "5":
-            console.log("Saliendo del programa");
-            break;
-        default:
-            alert("La opción elegida es incorrecta");
+    if (opcion === "1") {
+        const nuevaTarea = prompt("Ingrese una nueva tarea:");
+        agregarTareaNueva(nuevaTarea);
+    } else if (opcion === "2") {
+        const indiceEditar = parseInt(prompt("Ingrese el número de la tarea a editar:"));
+        const nuevaTareaEditar = prompt("Ingrese la nueva tarea:");
+        editarTarea(indiceEditar, nuevaTareaEditar);
+    } else if (opcion === "3") {
+        const indiceEliminar = parseInt(prompt("Ingrese el número de la tarea a eliminar:"));
+        eliminarTarea(indiceEliminar);
+    } else if (opcion === "4") {
+        mostrarTodasLasTareas();
     }
-}
+} while (opcion !== "5");
+
+alert("Saliendo del programa");
